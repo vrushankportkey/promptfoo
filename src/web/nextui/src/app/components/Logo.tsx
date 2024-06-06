73d7cc40edd4caba88bb455a9708f2cc3cdd58b1
Ian Webster <ianw_github@ianww.com>
2024-06-05 23:12:34 -0700

    tailwindy stuff

--- src/web/nextui/src/app/components/Logo.tsx
+++ src/web/nextui/src/app/components/Logo.tsx
@@ -5,8 +5,9 @@ import './Logo.css';
 
 export default function Logo() {
   return (
-    <Box className="logo">
-      <Image width={25} height={25} src="/logo.svg" alt="Promptfoo logo" /> <span>promptfoo</span>
-    </Box>
+    <div className="flex items-center gap-2 bg-gray-200 p-0 pr-8 rounded-md dark:bg-gray-800">
+      <img className="w-6 mt-0.5 dark:invert" src="/logo.svg" alt="Promptfoo logo" />
+      <span className="font-mono text-gray-800 dark:text-gray-200">promptfoo</span>
+    </div>
   );
 }
--- src/web/nextui/src/app/components/Navigation.tsx
+++ src/web/nextui/src/app/components/Navigation.tsx
@@ -12,7 +12,7 @@ import './Navigation.css';
 function NavLink({ href, label }: { href: string; label: string }) {
   const pathname = usePathname() || '';
   return (
-    <Link href={href} className={pathname.startsWith(href) ? 'active' : ''}>
+    <Link href={href} className={pathname.startsWith(href) ? 'font-bold' : ''}>
       {label}
     </Link>
   );
@@ -27,24 +27,24 @@ export default function Navigation({
 }) {
   if (process.env.NEXT_PUBLIC_NO_BROWSING) {
     return (
-      <Stack direction="row" spacing={2} className="nav">
+      <div className="flex flex-row p-1 pl-4 gap-4 bg-gray-200 mb-4 dark:bg-gray-800">
         <Logo />
         <DarkMode darkMode={darkMode} onToggleDarkMode={onToggleDarkMode} />
-      </Stack>
+      </div>
     );
   }
   return (
-    <Stack direction="row" spacing={2} className="nav">
+    <div className="flex flex-row p-1 pl-4 gap-4 bg-gray-200 mb-4 dark:bg-gray-800">
       <Logo />
       <NavLink href="/setup" label="New Eval" />
       <NavLink href="/eval" label="Evals" />
       <NavLink href="/prompts" label="Prompts" />
       <NavLink href="/datasets" label="Datasets" />
       <NavLink href="/progress" label="Progress" />
-      <div className="right-aligned">
+      <div className="flex items-center gap-4 ml-auto mr-2">
         {USE_SUPABASE ? <LoggedInAs /> : null}
         <DarkMode darkMode={darkMode} onToggleDarkMode={onToggleDarkMode} />
       </div>
-    </Stack>
+    </div>
   );
 }
